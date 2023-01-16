975742423aadf8eb409cb5767d7c24150cadcaa4
suxxee <andresuhe80@gmail.com>
2023-01-16 15:00:02 +0800

    Event listener uusgeh

---
+++ app.js
@@ -0,0 +1,16 @@
+var uiController = (function() {
+   
+  })();
+  
+  var financeController = (function() {
+
+  })();
+  
+  var appController = (function(uiController, financeController){
+    
+    document.querySelector('.add__btn').addEventListener("click", function(){
+
+    })
+
+  })(uiController, financeController);
+  
