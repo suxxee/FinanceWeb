e3b7e5e1de94c058552000cb5e2b6366b8d5c1d8
suxxee <andresuhe80@gmail.com>
2023-01-18 00:17:55 +0800

    Hereglegchiin utguud avah

--- app.js
+++ app.js
@@ -1,16 +1,76 @@
-var uiController = (function() {
-   
-  })();
-  
-  var financeController = (function() {
+// ------------Дэлгэцтэй ажиллах контроллер------------
 
-  })();
-  
-  var appController = (function(uiController, financeController){
-    
-    document.querySelector('.add__btn').addEventListener("click", function(){
+var uiController = (function() 
+{
 
-    })
+  var DOMstrings = 
+  {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn:".add__btn"
+  }
 
-  })(uiController, financeController);
+  return {
+    getInput: function()
+    {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function()
+    {
+      return DOMstrings;
+    }
+
+  };
+})();
+
+//--------------------Санхүүтэй ажиллах контроллер----------------------
+
+var financeController = (function() 
+{
+
+})();
+
+//-----------------------Холбогч контроллер-----------------------
+
+var appController = (function(uiController, financeController)
+{
+
+  var DOM = uiController.getDOMstrings();
+
+  var ctrlAddItem = function()
+  {
+    console.log(uiController.getInput());
+  }
+
+  var setupEventListeners = function()
+  {
+    document.querySelector(DOM.addBtn).addEventListener("click", function()
+    {
+      ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event){
+    if(event.keycode === 13 || event.which === 13 )
+      {
+       ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function()
+     {
+       setupEventListeners();
+     }
+  }
+
+})(uiController, financeController);
+
+appController.init();
   
